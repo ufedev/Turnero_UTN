d96246c67cb3f8a60373bc1e4ad02bb1cda9e56a
Federico Malfasi <federico.malfasi@gmail.com>
2025-08-28 19:09:53 -0300

    turnero paso1

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,32 @@ import Input from "./components/Input"
 
 const App = () => {
   // primero estados
+
   const [nombreCompleto, modificarNombreCompleto] = useState("")
   const [horario, modificarHorario] = useState("")
+  const [telefono, modificarTelefono] = useState("")
+
+
+  const enviarFormulario = (evento) => {
+    evento.preventDefault()
+    const horarioDate = new Date(horario)
+    if (horarioDate.getDay() === 0) {
+      console.log("Es domingo")
+    }
+
+  }
 
   return (
     <div
       className="w-full min-h-screen bg-slate-950 flex justify-center items-center gap-3 px-4 ">
       <Contenedor className="bg-slate-300" >
         <div>
-          <h2>Formulario</h2>
-          <form>
+          <h2 className='text-3xl font-black text-center mt-5' >Formulario</h2>
+          <form
+            className="flex flex-col gap-5 pt-5"
+            onSubmit={enviarFormulario}
+
+          >
             <Input
               name="Nombre Completo"
               placeholder="Nombre Completo"
@@ -23,7 +39,6 @@ const App = () => {
               onChange={
                 (evento) => {
 
-                  console.log(nombreCompleto)
                   modificarNombreCompleto(evento.target.value)
 
                 }
@@ -33,19 +48,30 @@ const App = () => {
             <Input
               name="Horario"
               type="datetime-local"
+              step="1200"
+
               value={horario}
               onChange={(evento) => {
-                console.log(horario)
                 modificarHorario(evento.target.value)
               }
               }
             />
 
-            {/* 
-              Telfono (number/tel)
-              Horario (Datetime)
-            
-            */}
+            <Input
+              type="number"
+              name="Telefono"
+              placeholder="Ingrese su telefono"
+              value={telefono}
+              onChange={(evento) => {
+                modificarTelefono(evento.target.value)
+              }}
+
+            />
+            <input
+              className="w-full p-2 text-center font-bold bg-slate-950 text-slate-100 rounded mt-5 cursor-pointer hover:bg-slate-300 transition-all"
+              type="submit"
+              value="Agendar"
+            />
           </form>
         </div>
       </Contenedor>
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,22 +1,26 @@
-const Input = ({ name, onChange, value, type, placeholder }) => {
-    return (
-        <div
-            className="flex flex-col gap-2"
-        >
-            <label htmlFor={name}>{name}:</label>
-            <input
+const Input =
+    ({ name, onChange, value, type, placeholder, min, max, step }) => {
+        return (
+            <div
+                className="flex flex-col gap-2"
+            >
+                <label htmlFor={name}>{name}:</label>
 
-                className='p-1 border-[1px] border-slate-400 rounded shadow'
-                name={name}
-                id={name}
-                type={type}
-                placeholder={placeholder}
-                value={value}
-                onChange={onChange}
+                <input
 
-            />
-        </div>
-    )
-}
+                    className='p-1 border-[1px] border-slate-400 rounded shadow'
+                    name={name}
+                    id={name}
+                    type={type}
+                    placeholder={placeholder}
+                    value={value}
+                    onChange={onChange}
+                    min={min}
+                    max={max}
+                    step={step}
+                />
+            </div>
+        )
+    }
 
 export default Input
